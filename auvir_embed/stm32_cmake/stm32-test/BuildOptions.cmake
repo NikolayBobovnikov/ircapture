ffa0ba34becf22729a4b415e1277af2f0a6ededa
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2015-08-16 22:23:25 +0300

    fixed paths to toolchain and cmake modules

--- auvir_embed/stm32_cmake/stm32-test/BuildOptions.cmake
+++ auvir_embed/stm32_cmake/stm32-test/BuildOptions.cmake
@@ -4,11 +4,12 @@
 #option(CMAKE_TOOLCHAIN_FILE "/home/nikolay/work/dev/auvir/auvir_embed/stm32-cmake/cmake/gcc_stm32.cmake")
 
 
-set(TOOLCHAIN_PREFIX /home/nikolay/work/dev/auvir/auvir_embed/toolchain/gcc-arm-none-eabi-4_9-2015q2 -DTARGET_TRIPLET=arm-none-eabi)
+#set(TOOLCHAIN_PREFIX /home/nikolay/work/dev/auvir/auvir_embed/toolchain/gcc-arm-none-eabi-4_9-2015q2)
+set(TOOLCHAIN_PREFIX ${CMAKE_CURRENT_LIST_DIR}/../../toolchain/gcc-arm-none-eabi-4_9-2015q2)
 set(TARGET_TRIPLET arm-none-eabi)
 set(STM32_CHIP STM32F103C8)
 set(STM32_FAMILY F1)
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} /home/nikolay/work/dev/auvir/auvir_embed/stm32-cmake/cmake)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR}/../cmake)
 set(CMAKE_TOOLCHAIN_FILE ${CMAKE_MODULE_PATH}/gcc_stm32.cmake)
 
 
