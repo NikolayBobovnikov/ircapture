78666c5f778bcb611c9e3122add0eff503f3419a
Nikolay Bobovnikov <nbobovnikov@yandex.ru>
2015-08-21 16:03:59 +0300

    daily update

--- auvir_embed/stm32_cmake/stm32test-SPL/main.c
+++ auvir_embed/stm32_cmake/stm32test-SPL/main.c
@@ -9,5 +9,16 @@ void _exit(void)
 }
 int main(void)
 {
+    //enable clock for the peripheral
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+   
     return 0;
 }
+
+
