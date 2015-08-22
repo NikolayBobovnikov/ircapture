98de0d63159d2ae0fdbe47e799964256499f8d6c
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2015-08-22 13:30:08 +0300

    update blink example

--- auvir_embed/stm32_cmake/stm32test-SPL/main.c
+++ auvir_embed/stm32_cmake/stm32test-SPL/main.c
@@ -1,4 +1,4 @@
-#include "stm32f10x.h"
+#include "stm32f10x.h" 
 
 void _exit(void)
 {
@@ -9,16 +9,43 @@ void _exit(void)
 }
 int main(void)
 {
-    //enable clock for the peripheral
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-   
-    return 0;
+ GPIO_InitTypeDef gpio;
+ RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+ GPIO_StructInit(&gpio);
+ gpio.GPIO_Pin = GPIO_Pin_9; // Green LED
+ gpio.GPIO_Mode = GPIO_Mode_Out_PP;
+ gpio.GPIO_Speed = GPIO_Speed_2MHz;
+ GPIO_Init(GPIOC, &gpio);
+ gpio.GPIO_Pin = GPIO_Pin_8; // Blue LED
+ GPIO_Init(GPIOC, &gpio);
+
+ while(1)
+
+ {
+
+  static int count=0;
+
+  static int i;
+
+  static int led_state=0;
+
+
+
+  for (i=0; i<1000000; ++i)
+  {
+      ;
+  }
+
+  GPIO_WriteBit(GPIOC, GPIO_Pin_9, led_state ? Bit_SET : Bit_RESET);
+
+     led_state = !led_state;
+
+     GPIO_WriteBit(GPIOC, GPIO_Pin_8, led_state ? Bit_SET : Bit_RESET);
+
+
+
+
+ }
+
 }
-
-
