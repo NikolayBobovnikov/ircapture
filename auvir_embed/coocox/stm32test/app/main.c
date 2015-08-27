158cd553ab9d0164a567b2bf6b3a53518f640846
unknown <nbobovnikov@Nbobovnikov.home.softech.ru>
2015-08-27 17:47:34 +0300

    add USART example for coocox project

--- auvir_embed/coocox/stm32test/app/main.c
+++ auvir_embed/coocox/stm32test/app/main.c
@@ -2,21 +2,7 @@
 #include "stm32f10x_conf.h"
 #include <stdio.h>
 
-void InitUSART()
-{
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
 
-    USART_InitTypeDef USART_InitStructure;
-    USART_InitStructure.USART_BaudRate = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    USART_Init(USART1, &USART_InitStructure);
-
-    USART_Cmd(USART1, ENABLE);
-}
 void initLED()
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
@@ -50,10 +36,10 @@ void print()
 	printf("Hello, World!!!");
 }
 
-int main(void)
+
+int run_led_example(void)
 {
 	initLED();
-	InitUSART();
 
 	while(1)
     {
@@ -67,3 +53,85 @@ int main(void)
 	    print();
     }
 }
+
+
+//Структуры для инициализации GPIOA и USART1
+GPIO_InitTypeDef    GPIO_InitStruct;
+USART_InitTypeDef    USART_InitStruct;
+
+void Init(void); //Объявление функции инициализации периферии
+void Usart1_Send_symbol(uint8_t); //Объявление функции передачи символа
+void Usart1_Send_String(char* str); //Объявление функции передачи строки
+char* str;
+uint8_t data;
+//Функция инициализации периферии
+void Init()
+{
+  //Включаем тактирование GPIOA, USART1 и альтернативных функций AFIO
+  RCC_APB2PeriphClockCmd((RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO), ENABLE);
+
+  //Инициализации вывода PA9 - USART1_Tx
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9; //Настройки вывода PA9
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz; //Скорость порта максимальная
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP; //Режим альтернативной функции, выход Push-Pull
+  GPIO_Init(GPIOA, &GPIO_InitStruct); //Заданные настройки сохраняем в регистрах GPIOА
+
+  //Инициализации вывода PA10 - USART1_Rx
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10; //Настройки вывода PA10
+  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_IN_FLOATING; //Input floating
+  GPIO_Init(GPIOA, &GPIO_InitStruct); //Заданные настройки сохраняем в регистрах GPIOА
+
+
+  //Инициализация USART1
+  USART_InitStruct.USART_BaudRate = 9600; //Скорость обмена 9600 бод
+  USART_InitStruct.USART_WordLength = USART_WordLength_8b; //Длина слова 8 бит
+  USART_InitStruct.USART_StopBits = USART_StopBits_1; //1 стоп-бит
+  USART_InitStruct.USART_Parity = USART_Parity_No ; //Без проверки четности
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None; //Без аппаратного контроля
+  USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx; //Включен передатчик и приемник USART1
+  USART_Init(USART1, &USART_InitStruct); //Заданные настройки сохраняем в регистрах USART1
+
+  USART_Cmd(USART1, ENABLE); //Включаем USART1
+}
+
+//Функция передачи символа
+void Usart1_Send_symbol(uint8_t data)
+{
+  while(!(USART1->SR & USART_SR_TC)); //Проверяем установку флага TC - завершения предыдущей передачи
+  USART1->DR = data; //Записываем значение в регистр данных - передаем символ
+}
+
+//Функция передачи строки через USART
+void Usart1_Send_String(char* str)
+{
+  uint8_t i=0;
+  while(str[i])
+  {
+    Usart1_Send_symbol(str[i]);
+    i++;
+  }
+  Usart1_Send_symbol('n');
+  Usart1_Send_symbol('r');
+}
+
+int run_usart_example()
+{
+  Init(); //Вызов функции инициализации периферии
+  //Передаем строку, сообщающую о готовности микроконтроллера к обмену данными
+  Usart1_Send_String("I'm ready");
+  while(1)
+  {
+    if((USART1->SR & USART_SR_RXNE)) //Проверяем поступление данных от компьютера
+    {
+      data = USART1->DR; //Считываем принятые данные
+      Usart1_Send_symbol(data); //И тут же отсылаем их обратно
+    }
+  }
+}
+
+int main(void)
+{
+	return run_usart_example();
+}
+
+
