7d93e8849052919428bd64520e8a220471dceea5
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2015-09-08 02:16:29 +0300

    update flash loading via openocd

--- auvir_embed/stm32_cmake/stm32test-SPL/build.sh
+++ auvir_embed/stm32_cmake/stm32test-SPL/build.sh
@@ -7,6 +7,7 @@ cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make
 
-st-flash write v1 stm32test  0x08000000
+#st-flash write v1 stm32test  0x08000000
 
 #openocd -f ../openocd.cfg
+openocd -f board/stm32vldiscovery.cfg -f interface/stlink-v1.cfg -c "init" -c "reset halt" -c "flash write_image erase stm32test" -c "reset run" -c "shutdown"
