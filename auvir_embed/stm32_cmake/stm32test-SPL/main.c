f7208c3665e7262659ca91414d17a08e12a1b09a
Nikolay Bobovnikov <nbobovnikov@yandex.ru>
2015-09-09 13:11:24 +0300

    blink led using registers; change post-build event for making bin and hex output files

--- auvir_embed/stm32_cmake/stm32test-SPL/main.c
+++ auvir_embed/stm32_cmake/stm32test-SPL/main.c
@@ -20,7 +20,7 @@ void delay(unsigned int ms)
 }
 int main(void)
 {
-
+/*
  GPIO_InitTypeDef gpio;
  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
  GPIO_StructInit(&gpio);
@@ -32,17 +32,48 @@ int main(void)
  gpio.GPIO_Mode = GPIO_Mode_Out_PP;
  gpio.GPIO_Speed = GPIO_Speed_2MHz;
  GPIO_Init(GPIOC, &gpio);
+*/
+
+    //====================================
+    // enable clock for gpio port C
+    //====================================
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    //====================================
+    // setup GPIO port C pin 8 for output
+    //====================================
+
+    // output mode push-pull - by default
+    //GPIOC->CRH |= (0 << 2);
+    //GPIOC->CRH |= (0 << 3);
+
+    // setup speed 2Mhz
+    GPIOC ->CRH &= ~GPIO_CRH_MODE8;
+    GPIOC ->CRH |= GPIO_CRH_MODE8_1;
+    GPIOC ->CRH &= ~GPIO_CRH_CNF8;
+
+    // gpio on
+    GPIOC->BRR |= GPIO_BRR_BR8;
+    GPIOC->BSRR |= GPIO_BSRR_BS8;
+    //GPIOC->BSRR = (1 << 8);
 
  while(1)
 
  {
 
+/*
   GPIO_WriteBit(GPIOC, GPIO_Pin_9, Bit_SET );
   GPIO_WriteBit(GPIOC, GPIO_Pin_8, Bit_RESET);
   delay(1000000);
   GPIO_WriteBit(GPIOC, GPIO_Pin_9, Bit_RESET );
   GPIO_WriteBit(GPIOC, GPIO_Pin_8, Bit_SET);
   delay(1000000);
- }
+*/
+GPIOC->BSRR |= GPIO_BSRR_BS8;
+delay(1000000);
+GPIOC->BRR |= GPIO_BRR_BR8;
+delay(1000000);
+ 
+}
 
 }
