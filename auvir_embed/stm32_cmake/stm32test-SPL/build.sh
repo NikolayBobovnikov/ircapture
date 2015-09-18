e604d5097b16c8eb3b080ff05eee46123232c8f5
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2015-09-19 01:16:24 +0300

    add debug and compile.json options to cmake project file; ignore autosave and temporary cmake files

--- auvir_embed/stm32_cmake/stm32test-SPL/build.sh
+++ auvir_embed/stm32_cmake/stm32test-SPL/build.sh
@@ -4,13 +4,13 @@ rm -fr ./build
 mkdir build
 cd build
 #CMAKE_EXPORT_COMPILE_COMMANDS=ON - for ycm_extra_conf in vim completer plugin
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG ..
 make
 
 #st-flash write v1 stm32test  0x08000000
 
 #openocd -f ../openocd.cfg
-openocd -f board/stm32vldiscovery.cfg -f interface/stlink-v1.cfg -c "init" -c "reset halt" -c "flash write_image erase stm32test" -c "reset run" -c "shutdown"
+#openocd -f board/stm32vldiscovery.cfg -f interface/stlink-v1.cfg -c "init" -c "reset halt" -c "flash write_image erase stm32test" -c "reset run" -c "shutdown"
 
 cp stm32test* ~/Yandex.Disk/dev/auvir/build
 
