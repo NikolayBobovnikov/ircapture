da3a244df4904cc002b983aba801e0175e7f2f8d
unknown <nbobovnikov@Nbobovnikov.home.softech.ru>
2015-11-11 19:38:56 +0300

    update for MPU6050 with i2c; issues with initialization

--- auvir_embed/stm32_cmake/stm32test-SPL/main.c
+++ auvir_embed/stm32_cmake/stm32test-SPL/main.c
@@ -1,4 +1,5 @@
 #include "stm32f10x.h"
+#include "stm32f10x_i2c.h"
 #include "mpu6050.h"
 
 const char* const example_string = "Hello World!\r\n\0";
@@ -95,7 +96,7 @@ void init_uart()
     place. Configure the DMA register as explained in multibuffer communication.*/
         USART1->CR3 &= (~USART_CR3_DMAT);           // DMA disabled
     //5. Select the desired baud rate using the USART_BRR register.
-        USART1->BRR = (SystemCoreClock / 115200);   //115200 bps
+        USART1->BRR = (SystemCoreClock / 115200);   //9600 or 115200 bps
     //6. Set the TE bit in USART_CR1 to send an idle frame as first transmission.
         USART1->CR1 |= USART_CR1_TE;
 
@@ -150,95 +151,258 @@ void button_led()
         unsigned int is_button_off = (GPIOA->IDR & 0x1);
         if( is_button_off == 0)
         {
-            blue_led_off();
+            blue_led_on();
         }
         else
         {
-            blue_led_on();
+            blue_led_off();
             usart_send_str((uint8_t *)example_string);
         }
     }
 }
 
-void MPU6050_Initialize(void)
+
+
+/// i2c functions
+/*
+
+void I2C_LowLevel_Init(I2C_TypeDef* I2Cx , int ClockSpeed , int
+                       OwnAddress)
 {
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_PWR_MGMT_1, 1<<7);//reset the whole module first
-
-delay(50);	//wait for 50ms for the gyro to stable
-
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_PWR_MGMT_1, MPU6050_CLOCK_PLL_ZGYRO);//PLL with Z axis gyroscope reference
-
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_CONFIG, 0x01);		//DLPF_CFG = 1: Fs=1khz; bandwidth=42hz
-
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_SMPLRT_DIV, 0x01);	//500Hz sample rate ~ 2ms
-
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_GYRO_CONFIG, MPU6050_GYRO_FS_2000);	//Gyro full scale setting
-
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_ACCEL_CONFIG, MPU6050_ACCEL_FS_16);	//Accel full scale setting
-
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_INT_PIN_CFG, 1<<4);		//interrupt status bits are cleared on any read operation
-
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_INT_ENABLE, 1<<0);		//interupt occurs when data is ready. The interupt routine is in the receiver.c file.
-
-MPU6050_Write(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_SIGNAL_PATH_RESET, 0x07);//reset gyro and accel sensor
+    GPIO_InitTypeDef GPIO_InitStructure;
+    I2C_InitTypeDef I2C_InitStructure;
+    // Enable GPIOB clocks
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB , ENABLE);
+    // Configure I2C clock and GPIO
+    GPIO_StructInit (& GPIO_InitStructure);
+    if (I2Cx == I2C1){
+        // I2C1 clock enable
+        RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1 , ENABLE);
+        // I2C1 SDA and SCL configuration
+        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+        GPIO_Init(GPIOB , &GPIO_InitStructure);
+        // I2C1 Reset
+        RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1 , ENABLE);
+        RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1 , DISABLE);
+    }
+    else
+    {
+        // I2C2 ...
+    }
+    // Configure I2Cx
+    I2C_StructInit (& I2C_InitStructure);
+    I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+    I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
+    I2C_InitStructure.I2C_OwnAddress1 = OwnAddress;
+    I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+    I2C_InitStructure.I2C_AcknowledgedAddress =
+            I2C_AcknowledgedAddress_7bit;
+    I2C_InitStructure.I2C_ClockSpeed = ClockSpeed;
+    I2C_Init(I2Cx , &I2C_InitStructure);
+    I2C_Cmd(I2Cx , ENABLE);
 }
-void MPU6050_Write(uint8_t slaveAddr, uint8_t regAddr, uint8_t data)
+
+#define Timed(x) Timeout = 0xFFFF; while (x) \
+{ if (Timeout -- == 0) goto errReturn ;}
+
+
+ErrorStatus I2C_Write(I2C_TypeDef* I2Cx , const uint8_t* buf ,
+                 uint32_t nbyte , uint8_t SlaveAddress)
 {
-    uint8_t tmp;
-    tmp = data;
-    MPU6050_I2C_ByteWrite(slaveAddr,&tmp,regAddr);
+    __IO uint32_t Timeout = 0;
+    if (nbyte)
+    {
+        Timed(I2C_GetFlagStatus(I2Cx , I2C_FLAG_BUSY));
+        // Intiate Start Sequence
+        I2C_GenerateSTART(I2Cx , ENABLE);
+        Timed (! I2C_CheckEvent(I2Cx , I2C_EVENT_MASTER_MODE_SELECT));
+        // Send Address EV5
+        I2C_Send7bitAddress(I2Cx , SlaveAddress ,
+                            I2C_Direction_Transmitter);
+        Timed (! I2C_CheckEvent(I2Cx ,
+                                I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+        // EV6 Write first byte EV8_1
+        I2C_SendData(I2Cx , *buf ++);
+        while (--nbyte) {
+            // wait on BTF
+            Timed (! I2C_GetFlagStatus(I2Cx , I2C_FLAG_BTF));
+            I2C_SendData(I2Cx , *buf ++);
+        }
+        Timed (! I2C_GetFlagStatus(I2Cx , I2C_FLAG_BTF));
+        I2C_GenerateSTOP(I2Cx , ENABLE);
+        Timed(I2C_GetFlagStatus(I2C1 , I2C_FLAG_STOPF));
+    }
+    return SUCCESS;
+errReturn:
+    return ERROR;
 }
-//------------------------------------------------------------------
-void MPU6050_I2C_ByteWrite(u8 slaveAddr, u8* pBuffer, u8 writeAddr)
+
+ErrorStatus I2C_Read(I2C_TypeDef* I2Cx , uint8_t *buf ,
+                uint32_t nbyte , uint8_t SlaveAddress)
 {
+    __IO uint32_t Timeout = 0;
+    if (! nbyte)
+        return SUCCESS;
+    // Wait for idle I2C interface
+    Timed(I2C_GetFlagStatus(I2Cx , I2C_FLAG_BUSY));
+    // Enable Acknowledgment , clear POS flag
+    I2C_AcknowledgeConfig(I2Cx , ENABLE);
+    I2C_NACKPositionConfig(I2Cx , I2C_NACKPosition_Current);
+    // Intiate Start Sequence (wait for EV5)
+    I2C_GenerateSTART(I2Cx , ENABLE);
+    Timed (! I2C_CheckEvent(I2Cx , I2C_EVENT_MASTER_MODE_SELECT));
+    // Send Address
+    I2C_Send7bitAddress(I2Cx , SlaveAddress , I2C_Direction_Receiver);
+    // EV6
+    Timed (! I2C_GetFlagStatus(I2Cx , I2C_FLAG_ADDR));
 
-/* Send START condition */
-I2C_GenerateSTART(MPU6050_I2C, ENABLE);
-/* Test on EV5 and clear it */
-while(!I2C_CheckEvent(MPU6050_I2C, I2C_EVENT_MASTER_MODE_SELECT));
-/* Send MPU6050 address for write */
-I2C_Send7bitAddress(MPU6050_I2C, slaveAddr, I2C_Direction_Transmitter);
-/* Test on EV6 and clear it */
-while(!I2C_CheckEvent(MPU6050_I2C, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-/* Send the MPU6050's internal address to write to */
-I2C_SendData(MPU6050_I2C, writeAddr);
-/* Test on EV8 and clear it */
-//while(!I2C_CheckEvent(MPU6050_I2C, I2C_EVENT_MASTER_BYTE_TRANSMITTING));
-/* Send the byte to be written */
-if (pBuffer!=0) I2C_SendData(MPU6050_I2C, pBuffer);
-/* Test on EV8_2 and clear it */
-while(!I2C_CheckEvent(MPU6050_I2C, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-/* Send STOP condition */
-I2C_GenerateSTOP(MPU6050_I2C, ENABLE);
-
+    // process 1, 2 and more bytes differently
+    if (nbyte == 1)
+    {
+        // Clear Ack bit
+        I2C_AcknowledgeConfig(I2Cx , DISABLE);
+        // EV6_1 -- must be atomic -- Clear ADDR , generate STOP
+        __disable_irq ();
+        (void) I2Cx ->SR2;
+        I2C_GenerateSTOP(I2Cx ,ENABLE);
+        __enable_irq ();
+        // Receive data EV7
+        Timed (! I2C_GetFlagStatus(I2Cx , I2C_FLAG_RXNE));
+        *buf++ = I2C_ReceiveData(I2Cx);
+    }
+    else if (nbyte == 2)
+    {
+        // Set POS flag
+        I2C_NACKPositionConfig(I2Cx , I2C_NACKPosition_Next);
+        // EV6_1 -- must be atomic and in this order
+        __disable_irq ();
+        (void) I2Cx ->SR2; // Clear ADDR flag
+        I2C_AcknowledgeConfig(I2Cx , DISABLE); // Clear Ack bit
+        __enable_irq ();
+        // EV7_3 -- Wait for BTF , program stop , read data twice
+        Timed (! I2C_GetFlagStatus(I2Cx , I2C_FLAG_BTF));
+        __disable_irq ();
+        I2C_GenerateSTOP(I2Cx ,ENABLE);
+        *buf++ = I2Cx ->DR;
+        __enable_irq ();
+        *buf++ = I2Cx ->DR;
+    }
+    else {
+        (void) I2Cx ->SR2; // Clear ADDR flag
+        while (nbyte -- != 3)
+        {
+            // EV7 -- cannot guarantee 1 transfer completion time ,
+            // wait for BTF instead of RXNE
+            Timed (! I2C_GetFlagStatus(I2Cx , I2C_FLAG_BTF));
+            *buf++ = I2C_ReceiveData(I2Cx);
+        }
+        Timed (! I2C_GetFlagStatus(I2Cx , I2C_FLAG_BTF));
+        // EV7_2 -- Figure 1 has an error , doesn 't read N-2 !
+        I2C_AcknowledgeConfig(I2Cx , DISABLE); // clear ack bit
+        __disable_irq ();
+        *buf++ = I2C_ReceiveData(I2Cx); // receive byte N-2
+        I2C_GenerateSTOP(I2Cx ,ENABLE); // program stop
+        __enable_irq ();
+        *buf++ = I2C_ReceiveData(I2Cx); // receive byte N-1
+        // wait for byte N
+        Timed (! I2C_CheckEvent(I2Cx , I2C_EVENT_MASTER_BYTE_RECEIVED));
+        *buf++ = I2C_ReceiveData(I2Cx);
+        nbyte = 0;
+    }
+    // Wait for stop
+    Timed(I2C_GetFlagStatus(I2Cx , I2C_FLAG_STOPF));
+    return SUCCESS;
+errReturn:
+    return ERROR;
 }
-NVIC_InitTypeDef NVIC_InitStructure;
-DMA_InitTypeDef  DMA_InitStructure;
 
-DMA_DeInit(MPU6050_DMA_Channel); //reset DMA1 channe1 to default values;
-
-DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)I2C_DR_Address; //=0x40005410 : address of data reading register of I2C1
-DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)I2C_Rx_Buffer; //variable to store data
-DMA_InitStructure.DMA_M2M = DMA_M2M_Disable; //channel will be used for peripheral to memory transfer
-DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;	//setting normal mode (non circular)
-DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;	//medium priority
-DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;	//Location assigned to peripheral register will be source
-DMA_InitStructure.DMA_BufferSize = 14;	//number of data to be transfered
-DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable; //automatic memory increment disable for peripheral
-DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;	//automatic memory increment enable for memory
-DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;	//source peripheral data size = 8bit
-DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;	//destination memory data size = 8bit
-DMA_Init(MPU6050_DMA_Channel, &DMA_InitStructure);
-DMA_ITConfig(MPU6050_DMA_Channel, DMA_IT_TC, ENABLE);
-
-NVIC_InitStructure.NVIC_IRQChannel = DMA1_Channel7_IRQn; //I2C1 connect to channel 7 of DMA1
-NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x05;
-NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x05;
-NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-NVIC_Init(&NVIC_InitStructure);
+/// i2c functions
+*/
 
 int main()
 {
-    button_led();
+    //button_led();
+    init_uart();
+
+    // wait to press button
+    bool button_pressed = false;
+    while(!button_pressed)
+    {
+        unsigned int is_button_off = (GPIOA->IDR & 0x1);
+        if( is_button_off != 0)
+        {
+            button_pressed = true;
+        }
+    }
+
+    usart_send_str("Initialize led\r\n\0");
+    init_led();
+
+    usart_send_str("Initialize button\r\n\0");
+    init_button();
+
+
+    /*
+    // Init
+    #define MPU6050_ADDRESS 0x69
+    const uint8_t buf[] = {0xf0 , 0x55};
+    const uint8_t buf2[] = {0xfb , 0x00};
+    I2C_Write(I2C1 , buf , 2, MPU6050_ADDRESS);
+    I2C_Write(I2C1 , buf2 , 2, MPU6050_ADDRESS);
+
+    // Read
+    uint8_t data [6];
+    const uint8_t buf3[] = {0};
+    I2C_Write(I2C1 , buf3 , 1, MPU6050_ADDRESS);
+    I2C_Read(I2C1 , data , 6, MPU6050_ADDRESS);
+    */
+
+    usart_send_str("MPU6050_I2C_Init\r\n\0");
+    //MPU6050_I2C_Init();
+
+    usart_send_str("MPU6050_Initialize\r\n\0");
+    //MPU6050_Initialize();
+    //MPU6050_Write();
+    //MPU6050_Read();
+
+
+    const char* const msg_conenction_success = "Connected!\r\n\0";
+    const char* const msg_conenction_fail = "Oops, connection failed.\r\n\0";
+
+
+    while(1)
+    {
+        unsigned int is_button_off = (GPIOA->IDR & 0x1);
+        bool message_sent = false;
+        if( is_button_off == 0)
+        {
+            blue_led_on();
+        }
+        else
+        {
+            blue_led_off();
+            usart_send_str((uint8_t *)example_string);
+            /*
+            if(!message_sent)
+            {
+                // test connection and send a message
+                if(MPU6050_TestConnection())
+                {
+                    usart_send_str((uint8_t *)msg_conenction_success);
+                }
+                else
+                {
+                    usart_send_str((uint8_t *)msg_conenction_fail);
+                }
+                usart_send_str((uint8_t *)example_string);
+                message_sent = true;
+            }
+            */
+        }
+    }
+
+
     return 0;
 }
