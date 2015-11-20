baf8a32f353888f83375770b96afcefa05db3c25
unknown <nbobovnikov@Nbobovnikov.home.softech.ru>
2015-11-20 15:04:47 +0300

    update HAL project; revert back SPL project

--- auvir_embed/stm32_cmake/stm32test-SPL/stm32f10x_it.h
+++ auvir_embed/stm32_cmake/stm32test-SPL/stm32f10x_it.h
@@ -44,6 +44,12 @@
  void DebugMon_Handler(void);
  void PendSV_Handler(void);
  void SysTick_Handler(void);
+ void I2C1_EV_IRQHandler(void);
+ void I2C1_ER_IRQHandler(void);
+ void I2C2_EV_IRQHandler(void);
+ void I2C2_ER_IRQHandler(void);
+ void DMA1_Channel6_IRQHandler(void);
+ void DMA1_Channel7_IRQHandler(void);
 
 #ifdef __cplusplus
 }
