cefc9b1d491341d2c0681a0bdf2d3bdb124e72dd
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2016-01-22 23:45:41 +0300

    add clang-format to environment setup script; test formatting applied

--- auvir_embed/mocap/transmitter/stm32f103/Src/main.c
+++ auvir_embed/mocap/transmitter/stm32f103/Src/main.c
@@ -6,26 +6,33 @@
   *
   * COPYRIGHT(c) 2016 STMicroelectronics
   *
-  * Redistribution and use in source and binary forms, with or without modification,
+  * Redistribution and use in source and binary forms, with or without
+  *modification,
   * are permitted provided that the following conditions are met:
   *   1. Redistributions of source code must retain the above copyright notice,
   *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
+  *   2. Redistributions in binary form must reproduce the above copyright
+  *notice,
+  *      this list of conditions and the following disclaimer in the
+  *documentation
   *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *   3. Neither the name of STMicroelectronics nor the names of its
+  *contributors
   *      may be used to endorse or promote products derived from this software
   *      without specific prior written permission.
   *
   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
   * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  *ARE
   * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
   * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
   * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
   * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+  *LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+  *USE
   * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
@@ -54,21 +61,21 @@ UART_HandleTypeDef huart1;
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
 
-//TODO: cleanup when done debugging
+// TODO: cleanup when done debugging
 const bool _debug = true;
 
-TIM_HandleTypeDef * phtim_envelop = &htim3;
-TIM_HandleTypeDef * phtim_pwm = &htim4;
+TIM_HandleTypeDef *phtim_envelop = &htim3;
+TIM_HandleTypeDef *phtim_pwm = &htim4;
 
 /// ======================= USART stuff =======================
 HAL_StatusTypeDef status;
 extern DataFrame_t tx_data_frame;
 
 enum UART_Commands {
-    UART_COMMAND_NOT_RECEIVED = 0,
-    UART_DEBUG_DATA_TRANSMIT,
-    UART_DEBUG_DATA_TRANSMIT_OK,
-    UART_ECHO
+  UART_COMMAND_NOT_RECEIVED = 0,
+  UART_DEBUG_DATA_TRANSMIT,
+  UART_DEBUG_DATA_TRANSMIT_OK,
+  UART_ECHO
 };
 uint8_t command = UART_COMMAND_NOT_RECEIVED;
 uint8_t responce = UART_DEBUG_DATA_TRANSMIT_OK;
@@ -94,16 +101,16 @@ void notify_transmission_finished();
 /* USER CODE BEGIN 0 */
 /* USER CODE END 0 */
 
-int main(void)
-{
-
+int main(void) {
   /* USER CODE BEGIN 1 */
   //  htim_envelop = htim1;
   /* USER CODE END 1 */
 
-  /* MCU Configuration----------------------------------------------------------*/
+  /* MCU
+   * Configuration----------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* Configure the system clock */
@@ -120,63 +127,54 @@ int main(void)
   MX_USART1_UART_Init();
 
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(phtim_envelop); // envelop
-  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4); // pwm
+  HAL_TIM_Base_Start_IT(phtim_envelop);      // envelop
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);  // pwm
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-      send_data();
-      /*
-      command = UART_COMMAND_NOT_RECEIVED;
-      status = HAL_UART_Receive(&huart1, &command, sizeof(command), 1000);
+  while (1) {
+    // send_data();
 
-      if(status != HAL_OK)
-      {
+    command = UART_COMMAND_NOT_RECEIVED;
+    status = HAL_UART_Receive(&huart1, &command, sizeof(command), 1000);
+
+    if (status != HAL_OK) {
+      // TODO: process error
+      break;
+    }
+
+    // if received command, dispatch it
+    switch (command) {
+      case UART_DEBUG_DATA_TRANSMIT: {
+        // receive data to transmit
+        status = HAL_UART_Receive(&huart1, (uint8_t *)&tx_data_frame,
+                                  sizeof(tx_data_frame), 1000);
+        if (status != HAL_OK) {
           // TODO: process error
           break;
+        }
+        // responce = HAL_UART_Transmit(&huart1, (uint8_t*)&responce,
+        // sizeof(responce), 1000);
+
+        // transmit data
+        send_data();
+
+        // notify that data has been sent (future callback - )
+        notify_transmission_finished();
       }
+    }
 
-      // if received command, dispatch it
-      switch(command)
-      {
-          case UART_DEBUG_DATA_TRANSMIT:
-          {
-              // receive data to transmit
-              status = HAL_UART_Receive(&huart1, (uint8_t*)&tx_data_frame, sizeof(tx_data_frame), 1000);
-              if(status != HAL_OK)
-              {
-                  // TODO: process error
-                  break;
-              }
-              //responce = HAL_UART_Transmit(&huart1, (uint8_t*)&responce, sizeof(responce), 1000);
+    /* USER CODE END WHILE */
 
-              // transmit data
-              send_data();
-
-              // notify that data has been sent (future callback - )
-              notify_transmission_finished();
-          }
-
-      }
-      */
-
-  /* USER CODE END WHILE */
-
-  /* USER CODE BEGIN 3 */
-
+    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
-
 }
 
 /** System Clock Configuration
 */
-void SystemClock_Config(void)
-{
-
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
@@ -188,14 +186,14 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
   HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
 
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
 
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -204,18 +202,13 @@ void SystemClock_Config(void)
 }
 
 /* CRC init function */
-void MX_CRC_Init(void)
-{
-
+void MX_CRC_Init(void) {
   hcrc.Instance = CRC;
   HAL_CRC_Init(&hcrc);
-
 }
 
 /* SPI1 init function */
-void MX_SPI1_Init(void)
-{
-
+void MX_SPI1_Init(void) {
   hspi1.Instance = SPI1;
   hspi1.Init.Mode = SPI_MODE_MASTER;
   hspi1.Init.Direction = SPI_DIRECTION_2LINES;
@@ -228,14 +221,12 @@ void MX_SPI1_Init(void)
   hspi1.Init.TIMode = SPI_TIMODE_DISABLED;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLED;
   hspi1.Init.CRCPolynomial = 10;
-  HAL_SPI_Init(&hspi1);
 
+  HAL_SPI_Init(&hspi1);
 }
 
 /* SPI2 init function */
-void MX_SPI2_Init(void)
-{
-
+void MX_SPI2_Init(void) {
   hspi2.Instance = SPI2;
   hspi2.Init.Mode = SPI_MODE_SLAVE;
   hspi2.Init.Direction = SPI_DIRECTION_2LINES;
@@ -249,13 +240,10 @@ void MX_SPI2_Init(void)
   hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLED;
   hspi2.Init.CRCPolynomial = 10;
   HAL_SPI_Init(&hspi2);
-
 }
 
 /* TIM3 init function */
-void MX_TIM3_Init(void)
-{
-
+void MX_TIM3_Init(void) {
   TIM_ClockConfigTypeDef sClockSourceConfig;
   TIM_MasterConfigTypeDef sMasterConfig;
 
@@ -272,13 +260,10 @@ void MX_TIM3_Init(void)
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig);
-
 }
 
 /* TIM4 init function */
-void MX_TIM4_Init(void)
-{
-
+void MX_TIM4_Init(void) {
   TIM_ClockConfigTypeDef sClockSourceConfig;
   TIM_MasterConfigTypeDef sMasterConfig;
   TIM_OC_InitTypeDef sConfigOC;
@@ -304,13 +289,10 @@ void MX_TIM4_Init(void)
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_4);
-
 }
 
 /* USART1 init function */
-void MX_USART1_UART_Init(void)
-{
-
+void MX_USART1_UART_Init(void) {
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -320,14 +302,12 @@ void MX_USART1_UART_Init(void)
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   HAL_UART_Init(&huart1);
-
 }
 
-/** 
+/**
   * Enable DMA controller clock
   */
-void MX_DMA_Init(void) 
-{
+void MX_DMA_Init(void) {
   /* DMA controller clock enable */
   __HAL_RCC_DMA1_CLK_ENABLE();
 
@@ -336,19 +316,16 @@ void MX_DMA_Init(void)
   HAL_NVIC_EnableIRQ(DMA1_Channel2_IRQn);
   HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
-
 }
 
-/** Configure pins as 
-        * Analog 
-        * Input 
+/** Configure pins as
+        * Analog
+        * Input
         * Output
         * EVENT_OUT
         * EXTI
 */
-void MX_GPIO_Init(void)
-{
-
+void MX_GPIO_Init(void) {
   GPIO_InitTypeDef GPIO_InitStruct;
 
   /* GPIO Ports Clock Enable */
@@ -356,25 +333,23 @@ void MX_GPIO_Init(void)
   __GPIOA_CLK_ENABLE();
   __GPIOB_CLK_ENABLE();
 
-    if(_debug)
-    {
-      /*Configure GPIO pins for debug : PB4, PB5 */
-      GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_5;
-      GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-      GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-      HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-    }
+  if (_debug) {
+    /*Configure GPIO pins for debug : PB4, PB5 */
+    GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  }
 }
 
 /* USER CODE BEGIN 4 */
-void notify_transmission_finished()
-{
-    responce = UART_DEBUG_DATA_TRANSMIT_OK;
-    HAL_StatusTypeDef status = HAL_UART_Transmit(&huart1, (uint8_t*)&responce, sizeof(responce), 1000);
-    if(status != HAL_OK)
-    {
-        // TODO: process error
-    }
+void notify_transmission_finished() {
+  responce = UART_DEBUG_DATA_TRANSMIT_OK;
+  HAL_StatusTypeDef status =
+      HAL_UART_Transmit(&huart1, (uint8_t *)&responce, sizeof(responce), 1000);
+  if (status != HAL_OK) {
+    // TODO: process error
+  }
 }
 /* USER CODE END 4 */
 
@@ -387,23 +362,22 @@ void notify_transmission_finished()
    * @param line: assert_param error line source number
    * @retval None
    */
-void assert_failed(uint8_t* file, uint32_t line)
-{
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
+  /* User can add his own implementation to report the file name and line
+    number,
     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
-
 }
 
 #endif
 
 /**
   * @}
-  */ 
+  */
 
 /**
   * @}
-*/ 
+*/
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
