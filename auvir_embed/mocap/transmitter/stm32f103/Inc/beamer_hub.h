25271d0d1199d8f81bfdee68fba860ed8c7353fd
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2016-01-28 00:06:31 +0300

    add types uart message and who_am_i (to distinquidh identical actions like requesting and ID from different actors, like sensor, sensorhub, beamer)

--- auvir_embed/mocap/transmitter/stm32f103/Inc/beamer_hub.h
+++ auvir_embed/mocap/transmitter/stm32f103/Inc/beamer_hub.h
@@ -5,6 +5,28 @@
 #include "stm32f1xx.h"
 #include "stm32f1xx_it.h"
 
+
+typedef enum{
+    WHOAMI_UNDEFINED,
+    SENSOR,
+    SENSOR_HUB,
+    BEAMER,
+    BEAMER_HUB
+} WHOAMI_t;
+
+typedef enum{
+    UART_MSG_UNDEFINED,
+    UART_MSG_REQUEST_ID,
+    UART_MSG_RESPONCE_ID,
+    UART_MSG_RESPONCE_REGISTRATION_CLOSED
+} MSG_t;
+
+typedef struct UART_MESSAGE{
+    MSG_t message_type;
+    WHOAMI_t who_am_i;
+    uint8_t data_size;
+}UART_FRAME_t;
+
 void process_beamer_hub_states();
 
 
