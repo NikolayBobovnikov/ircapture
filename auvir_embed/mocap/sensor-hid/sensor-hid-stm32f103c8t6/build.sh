190e47504197d6b783d0d2c32bedb77093723118
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2016-02-09 12:11:03 -0800

    add project for sensor-hid

---
+++ auvir_embed/mocap/sensor-hid/sensor-hid-stm32f103c8t6/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo START BUILDING
+rm -fr ./build
+mkdir build
+cd build
+#CMAKE_EXPORT_COMPILE_COMMANDS=ON - for ycm_extra_conf in vim completer plugin
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG ..
+make
+
+#st-flash write v2 stm32test  0x08000000
+
+#kill hanging openocd if any
+#ps axf | grep openocd | grep -v grep | awk '{print "kill -9 " $1}' | sh
+
+#openocd -f ../openocd.cfg
+openocd -f interface/stlink-v2.cfg -c "init" -c "reset halt" -c "flash write_image erase motionsensor_receiver" -c "reset run" -c "shutdown"
+
+#cp stm32* ~/Yandex.Disk/dev/auvir/build
+
---
+++ auvir_embed/mocap/sensor-hid/sensor-hid-stm32f103c8t6/debug_stlink.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo START BUILDING
+rm -fr ./build
+mkdir build
+cd build
+#CMAKE_EXPORT_COMPILE_COMMANDS=ON - for ycm_extra_conf in vim completer plugin
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG ..
+make
+
+cd ..
+
+#kill hanging openocd if any
+ps axf | grep openocd | grep -v grep | awk '{print "kill -9 " $1}' | sh
+
+#start debugger
+#(openocd -f ./openocd.cfg &)
+(st-util &)
+
+arm-none-eabi-gdb ./build/motionsensor_hid -command=./gdb_start_script
+
+#kill hanging openocd if any
+ps axf | grep openocd | grep -v grep | awk '{print "kill -9 " $1}' | sh
