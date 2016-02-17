a48707865e746113a79e3edc1ff3038170ad2b49
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2016-02-17 10:45:47 -0800

    regenerate USB project for CDC

--- auvir_embed/mocap/sensor-hid/sensor-hid-stm32f103c8t6/Src/usbd_desc.c
+++ auvir_embed/mocap/sensor-hid/sensor-hid-stm32f103c8t6/Src/usbd_desc.c
@@ -59,13 +59,13 @@
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "DeepImmersion"
-#define USBD_PID_FS     22352
-#define USBD_PRODUCT_STRING_FS     "STM32 IR Mocap Sensor HID"
+#define USBD_PID_FS     22336
+#define USBD_PRODUCT_STRING_FS     "STM32 iMocap Virtual ComPort"
 /* USER CODE BEGIN SERIALNUMBER_STRING_FS */
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
 /* USER CODE END SERIALNUMBER_STRING_FS */
-#define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
-#define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
+#define USBD_CONFIGURATION_STRING_FS     "CDC Config"
+#define USBD_INTERFACE_STRING_FS     "CDC Interface"
 
 /**
   * @}
