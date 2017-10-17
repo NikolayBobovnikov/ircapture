f63487c9ba640d6b1c5408f3a89efa68942f93f7
Nikolay Bobovnikov <nbobovnikov@Nbobovnikov.home.softech.ru>
2017-10-17 19:15:39 +0300

    update projects

---
+++ auvir_embed/mocap/vivetracker/openocd_flash.bat
@@ -0,0 +1,2 @@
+openocd.exe -f ./openocd.cfg -s D:\Software\OpenOCD-0.9.0-Win32\share\openocd\scripts -c "init" -c "reset halt" -c "flash write_image erase build/vivetracker" -c "reset run" -c "shutdown"
+
